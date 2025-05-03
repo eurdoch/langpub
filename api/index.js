66ef732e467743676d9aa0c8454886f3452bf99e
George Balch <george.balch@proton.me>
2025-05-03 19:10:03 +0000

    Change port

--- api/index.js
+++ api/index.js
@@ -331,7 +331,7 @@ app.post('/query', async (req, res) => {
 });
 
 
-app.listen(3002, () => {
-  console.log('Server listening on port 3002');
+app.listen(3004, () => {
+  console.log('Server listening on port 3004');
 });
 
